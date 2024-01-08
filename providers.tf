715331fb0ecb11153a83e0d4a0702f693243e539
chathu2004 <chathurasamerajeewa@gmail.com>
2024-01-09 01:03:07 +0530

    commit

---
+++ providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "chtu-tfstate-bkt"
+    key    = "terraform.tfstate"
+    region = "eu-north-1"
+    #dynamodb_table = "my-lock-table"
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+  # Credentials can be specified here or via environment variables
+}
